789f0a3b5285fce47e9d9423d9573bd3ccbf1e04
Aleksandr Vilchinskii <vilchinsky91@mail.ru>
2014-12-26 01:40:47 +0400

    deque: initial commit for ILogger

---
+++ code/vilchinskii-aleksandr/ViewModel/src/main/java/ru/unn/agile/Deque/viewmodel/ILogger.java
@@ -0,0 +1,25 @@
+package ru.unn.agile.Deque.viewmodel;
+
+import java.util.List;
+
+public interface ILogger {
+    void log(Level level, final String s);
+
+    List<String> getLog();
+
+    public enum Level {
+        ERROR("ERROR: "),
+        INFO("INFO: "),
+        DEBUG("DEBUG: ");
+
+        private final String name;
+
+        private Level(final String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return name;
+        }
+    }
+}
