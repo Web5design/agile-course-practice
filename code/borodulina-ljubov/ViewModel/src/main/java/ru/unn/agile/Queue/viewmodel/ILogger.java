27193c66d249d32297d2e8b4a88d81d01dea0449
Borodulina Ljubov <borodulinals@gmail.com>
2014-12-26 04:25:33 +0300

    queue: create ILogger

---
+++ code/borodulina-ljubov/ViewModel/src/main/java/ru/unn/agile/Queue/viewmodel/ILogger.java
@@ -0,0 +1,25 @@
+package ru.unn.agile.Queue.viewmodel;
+
+import java.util.List;
+
+public interface ILogger {
+    void log(Level level, final String s);
+
+    List<String> getLog();
+
+    public enum Level {
+        ERR("LOG_ERR: "),
+        INFO("LOG_INFO: "),
+        DBG("LOG_DBG: ");
+
+        private final String level;
+
+        private Level(final String level) {
+            this.level = level;
+        }
+
+        public String toString() {
+            return level;
+        }
+    }
+}
