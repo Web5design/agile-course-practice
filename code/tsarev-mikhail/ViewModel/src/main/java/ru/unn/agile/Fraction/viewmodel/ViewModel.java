547246e476573209814c799d4452fd27c032f900
Mikhail Tsarev <tsarev.m.s+git@gmail.com>
2015-02-21 22:33:22 +0300

    status by input tests

--- code/tsarev-mikhail/ViewModel/src/main/java/ru/unn/agile/Fraction/viewmodel/ViewModel.java
+++ code/tsarev-mikhail/ViewModel/src/main/java/ru/unn/agile/Fraction/viewmodel/ViewModel.java
@@ -2,26 +2,48 @@ package ru.unn.agile.Fraction.viewmodel;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import ru.unn.agile.Fraction.Model.Fraction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewModel {
     private final SimpleStringProperty resultNominator = new SimpleStringProperty();
     private final SimpleStringProperty resultNumerator = new SimpleStringProperty();
-    private final SimpleStringProperty firstNominator = new SimpleStringProperty();
+    private final SimpleStringProperty firstDenominator = new SimpleStringProperty();
     private final SimpleStringProperty firstNumerator = new SimpleStringProperty();
-    private final SimpleStringProperty secondNominator = new SimpleStringProperty();
+    private final SimpleStringProperty secondDenominator = new SimpleStringProperty();
     private final SimpleStringProperty secondNumerator = new SimpleStringProperty();
 
     private final StringProperty status = new SimpleStringProperty();
+
+    private final List<ValueChangeListener> valueChangedListeners = new ArrayList<>();
 
     // FXML needs default c-tor for binding
     public ViewModel() {
         resultNominator.set("");
         resultNumerator.set("");
-        firstNominator.set("");
+        firstDenominator.set("");
         firstNumerator.set("");
-        secondNominator.set("");
+        secondDenominator.set("");
         secondNumerator.set("");
         status.set(Status.WAITING.toString());
+
+        // Add listeners to the input text fields
+        final List<StringProperty> fields = new ArrayList<StringProperty>() { {
+            add(firstDenominator);
+            add(firstNumerator);
+            add(secondDenominator);
+            add(secondNumerator);
+        } };
+
+        for (StringProperty field : fields) {
+            final ValueChangeListener listener = new ValueChangeListener();
+            field.addListener(listener);
+            valueChangedListeners.add(listener);
+        }
     }
 
     public StringProperty resultNominatorProperty() {
@@ -32,16 +54,16 @@ public class ViewModel {
         return resultNumerator;
     }
 
-    public StringProperty firstNominatorProperty() {
-        return firstNominator;
+    public StringProperty firstDenominatorProperty() {
+        return firstDenominator;
     }
 
     public StringProperty firstNumeratorProperty() {
         return firstNumerator;
     }
 
-    public StringProperty secondNominatorProperty() {
-        return secondNominator;
+    public StringProperty secondDenominatorProperty() {
+        return secondDenominator;
     }
 
     public StringProperty secondNumeratorProperty() {
@@ -51,10 +73,57 @@ public class ViewModel {
     public StringProperty statusProperty() {
         return status;
     }
+
+    private Status getInputStatus() {
+        Status inputStatus = Status.READY;
+        if (firstDenominator.get().isEmpty() || firstNumerator.get().isEmpty()
+                || secondDenominator.get().isEmpty() || secondNumerator.get().isEmpty()) {
+            inputStatus = Status.WAITING;
+        }
+        try {
+            if (!firstDenominator.get().isEmpty()) {
+                Integer.parseInt(firstDenominator.get());
+            }
+            if (!firstNumerator.get().isEmpty()) {
+                Integer.parseInt(firstNumerator.get());
+            }
+            if (!secondDenominator.get().isEmpty()) {
+                Integer.parseInt(secondDenominator.get());
+            }
+            if (!secondNumerator.get().isEmpty()) {
+                Integer.parseInt(secondNumerator.get());
+            }
+        } catch (NumberFormatException nfe) {
+            inputStatus = Status.BAD_FORMAT;
+        }
+        try {
+            if (inputStatus == Status.READY) {
+                new Fraction(Integer.parseInt(firstNumerator.get()),
+                                Integer.parseInt(firstDenominator.get()));
+                new Fraction(Integer.parseInt(secondNumerator.get()),
+                                Integer.parseInt(secondDenominator.get()));
+            }
+        } catch (IllegalArgumentException iae) {
+            inputStatus = Status.IMPOSSIBLE_FRACTION;
+        }
+
+        return inputStatus;
+    }
+
+    private class ValueChangeListener implements ChangeListener<String> {
+        @Override
+        public void changed(final ObservableValue<? extends String> observable,
+                            final String oldValue, final String newValue) {
+            status.set(getInputStatus().toString());
+        }
+    }
 }
 
 enum Status {
-    WAITING("Please provide input data");
+    WAITING("Please provide input data"),
+    READY("Press 'Calculate'"),
+    BAD_FORMAT("Bad format"),
+    IMPOSSIBLE_FRACTION("Such fraction can not exists");
 
     private final String name;
     private Status(final String name) {
--- code/tsarev-mikhail/ViewModel/src/test/java/ru/unn/agile/Fraction/viewmodel/ViewModelTests.java
+++ code/tsarev-mikhail/ViewModel/src/test/java/ru/unn/agile/Fraction/viewmodel/ViewModelTests.java
@@ -26,14 +26,48 @@ public class ViewModelTests {
 
     @Test
     public void defaultArgumentsIsEmpty() {
-        assertEquals("", viewModel.firstNominatorProperty().get());
+        assertEquals("", viewModel.firstDenominatorProperty().get());
         assertEquals("", viewModel.firstNumeratorProperty().get());
-        assertEquals("", viewModel.secondNominatorProperty().get());
+        assertEquals("", viewModel.secondDenominatorProperty().get());
         assertEquals("", viewModel.secondNumeratorProperty().get());
     }
 
     @Test
     public void defaultStateIsWaiting() {
         assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusReadyWithGoodInput() {
+        setInputData();
+        assertEquals(Status.READY.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusWaitingWithClearField() {
+        setInputData();
+        viewModel.firstDenominatorProperty().set("");
+        assertEquals(Status.WAITING.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusBadFormatWithBadInput() {
+        setInputData();
+        viewModel.firstDenominatorProperty().set("trash");
+        assertEquals(Status.BAD_FORMAT.toString(), viewModel.statusProperty().get());
+    }
+
+    @Test
+    public void statusImpossibleWithBadInput() {
+        setInputData();
+        viewModel.firstDenominatorProperty().set("0");
+        assertEquals(Status.IMPOSSIBLE_FRACTION.toString(), viewModel.statusProperty().get());
+    }
+
+    private void setInputData() {
+        viewModel.firstDenominatorProperty().set("1");
+        viewModel.firstNumeratorProperty().set("2");
+        viewModel.secondDenominatorProperty().set("2");
+        viewModel.secondNumeratorProperty().set("3");
     }
 }
