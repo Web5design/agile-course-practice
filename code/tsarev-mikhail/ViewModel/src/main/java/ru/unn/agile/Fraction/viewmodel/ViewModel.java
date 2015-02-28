64f408e769d729af89d9b8d8e36169d2e7214360
Mikhail Tsarev <tsarev.m.s+git@gmail.com>
2015-02-28 18:38:02 +0300

    add comments

--- code/tsarev-mikhail/ViewModel/src/main/java/ru/unn/agile/Fraction/viewmodel/ViewModel.java
+++ code/tsarev-mikhail/ViewModel/src/main/java/ru/unn/agile/Fraction/viewmodel/ViewModel.java
@@ -14,6 +14,7 @@ import java.util.List;
 import ru.unn.agile.Fraction.Model.Fraction.Operation;
 
 public class ViewModel {
+    // ViewModel properties
     private final SimpleStringProperty resultDenominator = new SimpleStringProperty();
     private final SimpleStringProperty resultNumerator = new SimpleStringProperty();
     private final SimpleStringProperty firstNumerator = new SimpleStringProperty();
