22d9df1a7a8b2ed2a64f0e91aea24e1cb5eb4f57
izmailovajulia <luciferjulia@rambler.ru>
2015-03-01 01:40:30 +0400

    update

--- code/izmailova-julia/ViewModel/src/main/java/ru/unn/agile/Huffman/viewmodel/ViewModel.java
+++ code/izmailova-julia/ViewModel/src/main/java/ru/unn/agile/Huffman/viewmodel/ViewModel.java
@@ -6,6 +6,7 @@ public class ViewModel {
     private String codes;
     private String codeMessage;
     private String status;
+    
     public ViewModel() {
         message = "";
         codes = "";
